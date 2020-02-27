5f21d0f24136492117c570967604b013eb040a43
Deepakmalempati <deepakmalempati@gmail.com>
2020-02-27 15:59:04 -0600

    added new activity YourLisitngs

---
+++ app/src/main/java/com/example/roommatefinder/YourLisitngs.java
@@ -0,0 +1,14 @@
+package com.example.roommatefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourLisitngs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_lisitngs);
+    }
+}
