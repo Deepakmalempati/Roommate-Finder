ef92f0ea678a85b3885089de94693048cc10a71f
jyothsna5268 <s536775@nwmissouri.edu>
2020-02-29 21:58:36 -0600

    Added some changes in code to the  HomervAdapter.java and HomervModel.java

---
+++ app/src/main/java/com/example/roommatefinder/HomervModel.java
@@ -0,0 +1,42 @@
+package com.example.roommatefinder;
+
+import java.util.ArrayList;
+
+public class HomervModel {
+    public static class ChoiceInfo{
+        public String title;
+        public String location;
+        public int cost;
+
+
+        public ChoiceInfo(String title, String location,int cost ){
+            this.title=title;
+            this.location = location;
+            this.cost = cost;
+        }
+    }
+
+    public ArrayList<HomervModel.ChoiceInfo> choiceList;
+    private static HomervModel theModel=null;
+
+    private HomervModel(){
+        choiceList=new ArrayList<HomervModel.ChoiceInfo>();
+        loadModel();
+    }
+
+
+    public static HomervModel getSingleton(){
+        if(theModel==null)
+            theModel=new HomervModel();
+        return theModel;
+    }
+
+    private void loadModel(){
+
+        choiceList.add(new HomervModel.ChoiceInfo("Looking for roommate", "Maryville",290));
+
+
+    }
+
+
+}
