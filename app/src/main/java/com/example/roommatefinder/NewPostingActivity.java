60fda0b5c5109b8ce6f66e02e3db55e9c7c91fce
Deepakmalempati <deepakmalempati@gmail.com>
2020-03-02 18:02:59 -0600

    made UI changes

--- app/src/main/java/com/example/roommatefinder/NewPostingActivity.java
+++ app/src/main/java/com/example/roommatefinder/NewPostingActivity.java
@@ -2,7 +2,11 @@ package com.example.roommatefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NewPostingActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class NewPostingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_posting);
+    }
+
+
+    public void postBTNclick(View v){
+        Button postBTN = findViewById(R.id.postBTN);
+        Intent ini= new Intent(this,HomepageActivity.class);
+        startActivity(ini);
+        Toast toast=Toast.makeText(getApplicationContext(),"Posted Successfully", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
