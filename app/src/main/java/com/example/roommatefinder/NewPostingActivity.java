aab55a7624f4e9c4ce833b9ea99f9e4557e87ea3
Deepakmalempati <deepakmalempati@gmail.com>
2020-03-18 15:05:54 -0500

    added camera access to add images in NewPostingActivity.java

--- app/src/main/java/com/example/roommatefinder/NewPostingActivity.java
+++ app/src/main/java/com/example/roommatefinder/NewPostingActivity.java
@@ -3,13 +3,19 @@ package com.example.roommatefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class NewPostingActivity extends AppCompatActivity {
+
+    private static final int Image_Capture_Code = 1;
+    private ImageView imgCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,4 +58,21 @@ public class NewPostingActivity extends AppCompatActivity {
         Intent intent = new Intent(this,ProfileActivity.class);
         startActivity(intent);
     }
+
+   public void uploadimage(View view){
+       Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       startActivityForResult(cInt,Image_Capture_Code);
+   }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                imgCapture.setImageBitmap(bp);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
+
