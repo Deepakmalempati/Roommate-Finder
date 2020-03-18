d6e967d9c9f22aea098bb42a5d7c6a22dcf20841
Deepakmalempati <deepakmalempati@gmail.com>
2020-03-18 15:39:46 -0500

    added functionality to add profile picture from gallery

--- app/src/main/java/com/example/roommatefinder/ProfileActivity.java
+++ app/src/main/java/com/example/roommatefinder/ProfileActivity.java
@@ -2,18 +2,28 @@ package com.example.roommatefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        imageView = (ImageView)findViewById(R.id.imageView);
     }
 
     public void profileclick(View view){
@@ -23,6 +33,22 @@ public class ProfileActivity extends AppCompatActivity {
     public void homeBTNclick(View view){
         Intent intent = new Intent(this,HomepageActivity.class);
         startActivity(intent);
+    }
+
+    public void profilepic(View view){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            imageView.setBackground(getDrawable(R.color.white));
+        }
     }
 
 }
