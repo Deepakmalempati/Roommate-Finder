ac43ae8c880c1b562964087df18b9de14177a574
jeevan mure <s536980@nwmissouri.edu>
2020-03-21 20:42:43 -0500

    created method to reterive data from check boxes

--- app/src/main/java/com/example/roommatefinder/Filters.java
+++ app/src/main/java/com/example/roommatefinder/Filters.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 public class Filters extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
+    }
+    public void select(View view)
+    {
+        boolean checked = ((CheckBox) view).isChecked();
+
     }
 
     public void applyfiltersclick(View view){
