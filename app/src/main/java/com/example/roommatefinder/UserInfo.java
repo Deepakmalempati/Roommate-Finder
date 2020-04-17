84ab25cf4279fa0e1d9581e406a5c6632cb9978f
Abhiteja <S537489@nwmissouri.edu>
2020-04-17 18:05:45 -0500

    spaces removed in UserInfo.java

--- app/src/main/java/com/example/roommatefinder/UserInfo.java
+++ app/src/main/java/com/example/roommatefinder/UserInfo.java
@@ -21,39 +21,30 @@ public class UserInfo {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDob() {
         return dob;
     }
-
     public void setDob(String dob) {
         this.dob = dob;
     }
-
     public String getPlace() {
         return place;
     }
-
     public void setPlace(String place) {
         this.place = place;
     }
-
     public String getPhno() {
         return phno;
     }
-
     public void setPhno(String phno) {
         this.phno = phno;
     }
