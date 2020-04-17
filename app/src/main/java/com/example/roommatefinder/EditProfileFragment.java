ecf58af8e284a7d8adfdeae37cce9d3b627b5c91
Abhiteja <S537489@nwmissouri.edu>
2020-04-17 18:11:18 -0500

    changes made in EditProfileFragment.java

--- app/src/main/java/com/example/roommatefinder/EditProfileFragment.java
+++ app/src/main/java/com/example/roommatefinder/EditProfileFragment.java
@@ -1,14 +1,11 @@
 package com.example.roommatefinder;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,9 +19,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -137,17 +131,11 @@ private ProfileFragment profilefragment;
 //                String dob = dobETF.getText().toString();
 //                String place = placeETF.getText().toString();
 //                String phno = phnoETF.getText().toString();
-
-
+                
                  myActivity.SwapToEditProfileFragment();
-
-
 
             }
         });
-
-
-
 
         return v;
     }
