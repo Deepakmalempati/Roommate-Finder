8e3c6542c728eef240895009c4db46a9e6b816d9
Deepakmalempati <deepakmalempati@gmail.com>
2020-04-16 22:32:41 -0500

    Added permissions for internet

--- app/src/main/java/com/example/roommatefinder/listingsModel.java
+++ app/src/main/java/com/example/roommatefinder/listingsModel.java
@@ -1,59 +1,59 @@
-package com.example.roommatefinder;
-
-import java.util.ArrayList;
-
-import static com.example.roommatefinder.NewPostingActivity.listdataplace;
-import static com.example.roommatefinder.NewPostingActivity.listdataprice;
-import static com.example.roommatefinder.NewPostingActivity.listdatatitle;
-
-public class listingsModel {
-    public static class ChoiceInfo{
-        public String title;
-        public String location;
-        public String cost;
-
-
-        public ChoiceInfo(String title, String location,String cost ){
-            this.title=title;
-            this.location = location;
-            this.cost = cost;
-        }
-    }
-
-    public ArrayList<ChoiceInfo> choiceList;
-    private static listingsModel theModel=null;
-
-    private listingsModel(){
-        choiceList=new ArrayList<ChoiceInfo>();
-        loadModel();
-    }
-
-
-    public static listingsModel getSingleton(){
-        if(theModel==null)
-            theModel=new listingsModel();
-        return theModel;
-    }
-
-    public void loadModel(){
-
-    // choiceList.add(new ChoiceInfo("Looking for roommate", "Maryville","$290"));
-
-       //choiceList.add(new ChoiceInfo("Friendly roommate", "Kansas city","$290"));
-
-        choiceList.add(new ChoiceInfo("Female roommate", "Maryville","$290"));
-        choiceList.add(new ChoiceInfo("Male roommate", "Maryville","$290"));
-        choiceList.add(new ChoiceInfo("Male roommate111", "Maryville","$290"));
-       // choiceList.add(new ChoiceInfo(NewPostingActivity.gettitle(),NewPostingActivity.getplace(),NewPostingActivity.getprice()));
-
-        for(int i=0;i<listdatatitle.size();i++) {
-            choiceList.add(new ChoiceInfo(listdatatitle.get(i),listdataplace.get(i),listdataprice.get(i)));
-        }
-
-    }
-
-    public void reset(){
-        choiceList.clear();
-        loadModel();
-    }
-}
+//package com.example.roommatefinder;
+//
+//import java.util.ArrayList;
+//
+//import static com.example.roommatefinder.NewPostingActivity.listdataplace;
+//import static com.example.roommatefinder.NewPostingActivity.listdataprice;
+//import static com.example.roommatefinder.NewPostingActivity.listdatatitle;
+//
+//public class listingsModel {
+//    public static class ChoiceInfo{
+//        public String title;
+//        public String location;
+//        public String cost;
+//
+//
+//        public ChoiceInfo(String title, String location,String cost ){
+//            this.title=title;
+//            this.location = location;
+//            this.cost = cost;
+//        }
+//    }
+//
+//    public ArrayList<ChoiceInfo> choiceList;
+//    private static listingsModel theModel=null;
+//
+//    private listingsModel(){
+//        choiceList=new ArrayList<ChoiceInfo>();
+//        loadModel();
+//    }
+//
+//
+//    public static listingsModel getSingleton(){
+//        if(theModel==null)
+//            theModel=new listingsModel();
+//        return theModel;
+//    }
+//
+//    public void loadModel(){
+//
+//    // choiceList.add(new ChoiceInfo("Looking for roommate", "Maryville","$290"));
+//
+//       //choiceList.add(new ChoiceInfo("Friendly roommate", "Kansas city","$290"));
+//
+//        choiceList.add(new ChoiceInfo("Female roommate", "Maryville","$290"));
+//        choiceList.add(new ChoiceInfo("Male roommate", "Maryville","$290"));
+//        choiceList.add(new ChoiceInfo("Male roommate111", "Maryville","$290"));
+//       // choiceList.add(new ChoiceInfo(NewPostingActivity.gettitle(),NewPostingActivity.getplace(),NewPostingActivity.getprice()));
+//
+//        for(int i=0;i<listdatatitle.size();i++) {
+//            choiceList.add(new ChoiceInfo(listdatatitle.get(i),listdataplace.get(i),listdataprice.get(i)));
+//        }
+//
+//    }
+//
+//    public void reset(){
+//        choiceList.clear();
+//        loadModel();
+//    }
+//}
